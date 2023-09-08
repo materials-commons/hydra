b93b7475de2f3a9ed633b943ae6481e974e7c6c0
gtarcea <gtarcea@umich.edu>
2023-09-08 16:17:57 -0400

    Add factory to hide reuse of stors etc.., make references local to handlers

--- pkg/mcbridgefs/fs/mcfs/file_handle_factory.go
+++ pkg/mcbridgefs/fs/mcfs/file_handle_factory.go
@@ -1,34 +1,37 @@
 package mcfs
 
 import (
-	"crypto/md5"
-
 	"github.com/hanwen/go-fuse/v2/fs"
+	"github.com/materials-commons/hydra/pkg/mcbridgefs/fs/mcfs/projectpath"
 	"github.com/materials-commons/hydra/pkg/mcdb/mcmodel"
 	"github.com/materials-commons/hydra/pkg/mcdb/stor"
 )
 
-type FileHandleFactoryer interface {
-	NewFileHandle(fd int, file *mcmodel.File) fs.FileHandle
+type FileHandleFactory interface {
+	NewFileHandle(fd int, path string, file *mcmodel.File) fs.FileHandle
 }
 
-type MCFileHandlerFactory struct {
-	conversionStor      stor.ConversionStor
-	transferRequestStor stor.TransferRequestStor
+type LocalFileHandlerFactory struct {
+	conversionStor         stor.ConversionStor
+	transferRequestStor    stor.TransferRequestStor
+	activityCounterFactory *PathBasedActivityCounterFactory
 }
 
-func NewMCFileHandlerFactory(conversionStor stor.ConversionStor, transferRequestStor stor.TransferRequestStor) *MCFileHandlerFactory {
-	return &MCFileHandlerFactory{
-		conversionStor:      conversionStor,
-		transferRequestStor: transferRequestStor,
+func NewLocalFileHandlerFactory(conversionStor stor.ConversionStor, transferRequestStor stor.TransferRequestStor) *LocalFileHandlerFactory {
+	return &LocalFileHandlerFactory{
+		conversionStor:         conversionStor,
+		transferRequestStor:    transferRequestStor,
+		activityCounterFactory: NewPathBasedActivityCounterFactory(),
 	}
 }
 
-func (f *MCFileHandlerFactory) NewFileHandle(fd int, file *mcmodel.File) *LocalMonitoredFileHandle {
+func (f *LocalFileHandlerFactory) NewFileHandle(fd int, path string, file *mcmodel.File) *LocalMonitoredFileHandle {
+	projPath := projectpath.NewProjectPath(path)
+	activityCounter := f.activityCounterFactory.GetOrCreateActivityCounter(projPath.TransferBase)
 	return NewLocalMonitoredFileHandle(fd).
+		WithPath(path).
 		WithFile(file).
-		WithHasher(md5.New()).
-		WithActivityCounter(NewFSActivityCounter()).
+		WithActivityCounter(activityCounter).
 		WithConversionStor(f.conversionStor).
 		WithTransferRequestStor(f.transferRequestStor)
 }
