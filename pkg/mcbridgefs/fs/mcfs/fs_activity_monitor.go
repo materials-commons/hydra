b37af12ce32ed7235c4f220d53643285d1408c8c
gtarcea <gtarcea@umich.edu>
2023-09-08 16:18:42 -0400

    Add a factory so that we are only tracking activity off a project/user combination

--- pkg/mcbridgefs/fs/mcfs/fs_activity_monitor.go
+++ pkg/mcbridgefs/fs/mcfs/fs_activity_monitor.go
@@ -1,9 +1,56 @@
 package mcfs
 
 import (
+	"sync"
 	"sync/atomic"
 	"time"
 )
+
+type PathBasedActivityCounterFactory struct {
+	activityCounters map[string]*FSActivityCounter
+	mu               sync.Mutex
+}
+
+func NewPathBasedActivityCounterFactory() *PathBasedActivityCounterFactory {
+	return &PathBasedActivityCounterFactory{
+		activityCounters: make(map[string]*FSActivityCounter),
+	}
+}
+
+func (f *PathBasedActivityCounterFactory) GetOrCreateActivityCounter(path string) *FSActivityCounter {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	activityCounter, found := f.activityCounters[path]
+	if !found {
+		activityCounter = NewFSActivityCounter()
+		f.activityCounters[path] = activityCounter
+	}
+
+	return activityCounter
+}
+
+func (f *PathBasedActivityCounterFactory) GetActivityCounter(path string) *FSActivityCounter {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	activityCounter, found := f.activityCounters[path]
+
+	if !found {
+		return nil
+	}
+
+	return activityCounter
+}
+
+func (f *PathBasedActivityCounterFactory) ForEach(fn func(ac *FSActivityCounter)) {
+	f.mu.Lock()
+	defer f.mu.Unlock()
+
+	for _, activityCounter := range f.activityCounters {
+		fn(activityCounter)
+	}
+}
 
 type ActivityCounter interface {
 	IncrementActivityCount()
