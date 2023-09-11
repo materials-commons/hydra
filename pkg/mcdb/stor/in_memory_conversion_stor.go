917566b0f70b4a87349bb75e860d75976472e33a
gtarcea <gtarcea@umich.edu>
2023-09-11 17:05:45 -0400

    Implement in_memory_conversion_stor

--- pkg/mcdb/stor/in_memory_conversion_stor.go
+++ pkg/mcdb/stor/in_memory_conversion_stor.go
@@ -1,13 +1,48 @@
 package stor
 
-import "github.com/materials-commons/hydra/pkg/mcdb/mcmodel"
+import (
+	"time"
 
-type InMemoryConversionStor struct{}
+	"github.com/hashicorp/go-uuid"
+	"github.com/materials-commons/hydra/pkg/mcdb/mcmodel"
+)
+
+type InMemoryConversionStor struct {
+	// Can be used to simulate an error by setting it.
+	ErrorToReturn error
+	lastID        int
+}
 
 func NewInMemoryConversionStor() *InMemoryConversionStor {
-	return &InMemoryConversionStor{}
+	return &InMemoryConversionStor{lastID: 10000}
 }
 
 func (s *InMemoryConversionStor) AddFileToConvert(file *mcmodel.File) (*mcmodel.Conversion, error) {
-	return nil, nil
+	if s.ErrorToReturn != nil {
+		return nil, s.ErrorToReturn
+	}
+
+	var (
+		UUID string
+		err  error
+	)
+
+	if UUID, err = uuid.GenerateUUID(); err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	c := &mcmodel.Conversion{
+		ID:        s.lastID,
+		UUID:      UUID,
+		ProjectID: file.ProjectID,
+		OwnerID:   file.OwnerID,
+		FileID:    file.ID,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+
+	s.lastID = s.lastID + 1
+
+	return c, nil
 }
