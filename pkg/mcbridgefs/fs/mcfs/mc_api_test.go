fd96f8580f614e77024f78ca2b7206f480ec11bb
gtarcea <gtarcea@umich.edu>
2023-09-11 17:07:04 -0400

    Add placeholder tests and implement the first test for Create

---
+++ pkg/mcbridgefs/fs/mcfs/mc_api_test.go
@@ -0,0 +1,76 @@
+package mcfs
+
+import (
+	"testing"
+
+	"github.com/materials-commons/hydra/pkg/mcdb/mcmodel"
+	"github.com/materials-commons/hydra/pkg/mcdb/stor"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMCApi_Create(t *testing.T) {
+	files := []mcmodel.File{
+		{ID: 456, Path: "/data", ProjectID: 123, MimeType: "directory"},
+	}
+	transferRequests := []mcmodel.TransferRequest{
+		{ID: 234, ProjectID: 123, OwnerID: 301},
+	}
+	knownFilesTracker := NewKnownFilesTracker()
+	mcapi := NewMCApi(stor.NewInMemoryFileStor(files),
+		stor.NewInMemoryTransferRequestStor(transferRequests),
+		stor.NewInMemoryConversionStor(),
+		knownFilesTracker)
+
+	var tests = []struct {
+		name          string
+		path          string
+		f             *mcmodel.File
+		expectedError error
+	}{
+		{
+			name: "Test added file in knownFilesTracker",
+			path: "/123/301/data/file.txt",
+			f:    &mcmodel.File{ID: 123, Name: "file.txt"},
+		},
+	}
+
+	_ = mcapi
+	for _, test := range tests {
+		t.Run("", func(t *testing.T) {
+			createdFile, err := mcapi.Create(test.path)
+			if test.expectedError != nil {
+				require.Equal(t, test.expectedError, err)
+			} else {
+				require.Nil(t, err)
+				require.NotNil(t, createdFile)
+				f := knownFilesTracker.GetFile(test.path)
+				require.NotNil(t, f)
+				require.Equal(t, createdFile.ID, f.ID)
+			}
+		})
+	}
+}
+
+func TestMCApi_Open(t *testing.T) {
+
+}
+
+func TestMCApi_GetRealPath(t *testing.T) {
+
+}
+
+func TestMCApi_Lookup(t *testing.T) {
+
+}
+
+func TestMCApi_Mkdir(t *testing.T) {
+
+}
+
+func TestMCApi_Readdir(t *testing.T) {
+
+}
+
+func TestMCApi_Release(t *testing.T) {
+
+}
