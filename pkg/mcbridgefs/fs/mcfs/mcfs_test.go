468312a1f79cefd9ce35d1e49b31ad9cddabb1b4
gtarcea <gtarcea@umich.edu>
2023-09-17 16:18:36 -0400

    Add test for project that doesn't exist and test for checking a projects user

--- pkg/mcbridgefs/fs/mcfs/mcfs_test.go
+++ pkg/mcbridgefs/fs/mcfs/mcfs_test.go
@@ -7,11 +7,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestListingProjects(t *testing.T) {
-	tc := newTestCase(t, &fsTestOptions{mcfsDir: "/tmp/mcfs", mntDir: "/tmp/mnt/mcfs"})
+func TestListingTransferRequestProjects(t *testing.T) {
+	tc := newTestCase(t, &fsTestOptions{})
 	require.NotNil(t, tc)
 
 	entries, err := os.ReadDir("/tmp/mnt/mcfs")
+	require.NoError(t, err)
+	require.Len(t, entries, 1)
+	entry := entries[0]
+	require.True(t, entry.IsDir())
+	require.Equal(t, "1", entry.Name())
+
+	// Test that project doesn't exist
+	entries, err = os.ReadDir("/tmp/mnt/mcfs/2")
+	require.Error(t, err)
+}
+
+func TestListingTransferRequestProjectUsers(t *testing.T) {
+	tc := newTestCase(t, &fsTestOptions{})
+	require.NotNil(t, tc)
+
+	// newTestCase creates a single project that has id 1, with a single user
+	// with id 1
+	entries, err := os.ReadDir("/tmp/mnt/mcfs/1")
 	require.NoError(t, err)
 	require.Len(t, entries, 1)
 	entry := entries[0]
