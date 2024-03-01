1769c7eb17a5a1289a7904fb8752f1ccbd378c5b
gtarcea <gtarcea@umich.edu>
2024-03-01 15:57:28 -0500

    Initial prototype of mcdavd that just serves local files

--- cmd/mcdavd/cmd/root.go
+++ cmd/mcdavd/cmd/root.go
@@ -4,24 +4,49 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/net/webdav"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "hydra",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Use:   "mcdavd",
+	Short: "Run a WebDav server for Materials Commons",
+	Long:  `Run a WebDav server for Materials Commons`,
+	Run: func(cmd *cobra.Command, args []string) {
+		webdavSrv := &webdav.Handler{
+			Prefix:     "/webdav",
+			FileSystem: webdav.Dir("/home/gtarcea/Downloads"),
+			LockSystem: webdav.NewMemLS(),
+			Logger: func(r *http.Request, err error) {
+				if err != nil {
+					fmt.Printf("WebDAV %s: %s, ERROR: %s\n", r.Method, r.URL, err)
+				} else {
+					fmt.Printf("WebDAV %s: %s \n", r.Method, r.URL)
+				}
+			},
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Println("In HandleFunc")
+			username, password, _ := r.BasicAuth()
+			if username == "webdav@umich.edu" && password == "abc123" {
+				w.Header().Set("Timeout", "99999999")
+				webdavSrv.ServeHTTP(w, r)
+				return
+			}
+
+			w.Header().Set("WWW-Authenticate", `Basic realm="BASIC WebDAV REALM"`)
+			w.WriteHeader(401)
+			_, _ = w.Write([]byte("401 Unauthorized\n"))
+		})
+
+		_ = http.ListenAndServe(":8555", nil)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
